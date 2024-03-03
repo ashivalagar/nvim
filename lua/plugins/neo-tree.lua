90c06208b2622cf479b727280bcee3ca834b94dd
A Shiv Algar <ashivalagar@gmail.com>
2024-03-03 22:09:25 +0530

    init nvim config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+     vim.keymap.set('n', '<C-l>', ':Neotree filesystem reveal left<CR>',{}) 
+    end
+} 
